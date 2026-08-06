f57e0c1fc228fc92b2ead91e8253642bc85b94ef
CharlesVanEeckhout <vaneeckhoutcharles@gmail.com>
2026-08-06 19:24:24 -0400

    install.sh accept paths with spaces

--- install.sh
+++ install.sh
@@ -2,23 +2,23 @@
 set -e
 if [ "$1" != "" ]; then
 	if [ -d "$1" ]; then
-		mkdir -p $1/tools/agbcc
-		mkdir -p $1/tools/agbcc/bin
-		mkdir -p $1/tools/agbcc/include
-		mkdir -p $1/tools/agbcc/lib
-		cp agbcc $1/tools/agbcc/bin/
-		cp old_agbcc $1/tools/agbcc/bin/
-		cp agbcc_arm $1/tools/agbcc/bin/
-		cp -R libc/include $1/tools/agbcc/ #drop include, because we don't want include/include
-		cp ginclude/* $1/tools/agbcc/include/
-		cp libgcc.a $1/tools/agbcc/lib/
-		cp libc.a $1/tools/agbcc/lib/
+		mkdir -p "$1"/tools/agbcc
+		mkdir -p "$1"/tools/agbcc/bin
+		mkdir -p "$1"/tools/agbcc/include
+		mkdir -p "$1"/tools/agbcc/lib
+		cp agbcc "$1"/tools/agbcc/bin/
+		cp old_agbcc "$1"/tools/agbcc/bin/
+		cp agbcc_arm "$1"/tools/agbcc/bin/
+		cp -R libc/include "$1"/tools/agbcc/ #drop include, because we don't want include/include
+		cp ginclude/* "$1"/tools/agbcc/include/
+		cp libgcc.a "$1"/tools/agbcc/lib/
+		cp libc.a "$1"/tools/agbcc/lib/
 		echo "agbcc successfully installed!"
 	else
 		if [ -d "../$1" ]; then
 			echo "Target directory does not exist. Did you mean to do \"./install.sh ../$1\"?"
 		else
-			if case $1 in ".."*) true;; *) false;; esac; then
+			if case "$1" in ".."*) true;; *) false;; esac; then
 				echo "Target directory does not exist. If you aren't familiar with relative paths, make sure that agbcc and the repository are in the same directory, and run \"./install.sh $1\" again."
 			else
 				if echo "$1" | grep -qE '^[^/]*.$'; then
