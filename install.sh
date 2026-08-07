235e30e7e7d52a57a5068d421d1d147eab3d1507
CharlesVanEeckhout <vaneeckhoutcharles@gmail.com>
2026-08-06 20:58:54 -0400

    install.sh error msg move quotes around path

--- install.sh
+++ install.sh
@@ -16,13 +16,13 @@ if [ "$1" != "" ]; then
 		echo "agbcc successfully installed!"
 	else
 		if [ -d "../$1" ]; then
-			echo "Target directory does not exist. Did you mean to do \"./install.sh ../$1\"?"
+			printf 'Target directory does not exist. Did you mean to run ./install.sh "%s"?\n' "../$1"
 		else
 			if case "$1" in ".."*) true;; *) false;; esac; then
-				echo "Target directory does not exist. If you aren't familiar with relative paths, make sure that agbcc and the repository are in the same directory, and run \"./install.sh $1\" again."
+				printf 'Target directory does not exist. If you aren'\''t familiar with relative paths, make sure that agbcc and the repository are in the same directory, and run ./install.sh "%s" again.\n' "$1"
 			else
 				if echo "$1" | grep -qE '^[^/]*.$'; then
-					echo "Target directory does not exist. You probably meant to do \"./install.sh ../$1\", but agbcc and $1 do not exist in the same directory. Check your spelling, make sure that the repository has been cloned, ensure that agbcc and the repository are in the same directory, and run \"./install.sh ../$1\" again."
+					printf 'Target directory does not exist. You probably meant to do ./install.sh "../%s", but agbcc and %s do not exist in the same directory. Check your spelling, make sure that the repository has been cloned, ensure that agbcc and the repository are in the same directory, and run ./install.sh "../%s" again.\n' "$1" "$1" "$1"
 				else
 					echo "Target directory does not exist. Check your spelling, re-read the instructions, and try again."
 				fi
